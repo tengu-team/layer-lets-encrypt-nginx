d30e17ccfd337da78dd1b35e00c385d4c636f333
Mathijs Moerman <mathijs.moerman@qrama.io>
2017-06-29 15:23:52 +0200

    Supports multiple domain names per service, each with their own loadbalancing and basic auth. This through the use of subordinate charms per service

--- reactive/ssl_termination_proxy.py
+++ reactive/ssl_termination_proxy.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import os
-from subprocess import check_call
+from subprocess import check_call, CalledProcessError
 
 from charmhelpers.core import hookenv, unitdata
 from charmhelpers.core.hookenv import status_set
@@ -32,6 +32,8 @@ config = hookenv.config()
 @when('apt.installed.apache2-utils')
 @when_not('ssl-termination-proxy.installed')
 def install():
+    if not os.path.isdir('/etc/nginx/.htpasswd'):
+        os.mkdir('/etc/nginx/.htpasswd')
     set_state('ssl-termination-proxy.installed')
 
 
@@ -47,50 +49,65 @@ def check_status(ssltermination):
 
 
 @when('ssl-termination-proxy.installed', 'ssltermination.available')
-def setup_fqdns(ssltermination):
-    data = ssltermination.get_data()
-    fqdns = db.get('fqdns')
+def pre_setup(ssltermination):
     received_fqdns = []
-    for item in data:
-        received_fqdns.extend(item['fqdns'])
-    if fqdns is None:
+    received_basic_auth = []
+    received_private_ips = []
+    received_loadbalancing = []
+    fqdns = db.get('fqdns', [])
+    basic_auth = db.get('basic_auth', [])
+    private_ips = db.get('private_ips', [])
+    loadbalancing = db.get('loadbalancing', [])
+    for data in ssltermination.get_data():
+        received_fqdns.extend(data['fqdns'])
+        received_basic_auth.extend(data['basic_auth'])
+        received_private_ips.extend(data['private_ips'])
+        received_loadbalancing.append(data['loadbalancing'])
+    if fqdns != received_fqdns:
         db.set('fqdns', received_fqdns)
-    elif fqdns != received_fqdns:
-        db.set('fqdns', list(set(fqdns) | set(received_fqdns)))
         lets_encrypt.update_fqdns()
+        remove_state('ssl-termination-proxy.running')
+    if basic_auth != received_basic_auth:
+        db.set('basic_auth', received_basic_auth)
+        remove_state('ssl-termination-proxy.running')
+    if private_ips != received_private_ips:
+        db.set('private_ips', received_private_ips)
+        remove_state('ssl-termination-proxy.running')
+    if loadbalancing != received_loadbalancing:
+        db.set('loadbalancing', received_loadbalancing)
+        remove_state('ssl-termination-proxy.running')
 
 
 @when('ssl-termination-proxy.installed', 'ssltermination.available', 'lets-encrypt.registered')
-def set_up(ssltermination):
+@when_not('ssl-termination-proxy.running')
+def setup(ssltermination):
     print('SSL termination relation found, configuring proxy.')
-    data = ssltermination.get_data()[0]
-    service = data['service']
-    try:
-        os.remove('/etc/nginx/.htpasswd/{}'.format(service))
-    except OSError:
-        pass
-    # Did we get credentials? If so, configure them.
-    for user in data['basic_auth']:
-        check_call([
-            'htpasswd', '-b', '/etc/nginx/.htpasswd/{}'.format(service),
-            user['name'], user['password']])
-    live = lets_encrypt.live(data['fqdns'])
-    configure_site(
-        '{}.conf'.format(service), 'serivce.conf',
-        privkey=live['privkey'],
-        fullchain=live['fullchain'],
-        loadbalancing=data['loadbalancing'],
-        service=service,
-        servers=data['private_ips'],
-        fqdns=data['fqdns'],
-        dhparam=live['dhparam'],
-        auth_basic=bool(data['basic_auth']))
+    live = lets_encrypt.live(db.get('fqdns'))
+    for data in ssltermination.get_data():
+        service = data['service']
+        try:
+            os.remove('/etc/nginx/.htpasswd/{}'.format(service))
+        except OSError:
+            pass
+        # Did we get credentials? If so, configure them.
+        for user in data['basic_auth']:
+            try:
+                check_call([
+                    'htpasswd', '-b', '/etc/nginx/.htpasswd/{}'.format(service),
+                    user['name'], user['password']])
+            except CalledProcessError:
+                check_call([
+                    'htpasswd', '-bc', '/etc/nginx/.htpasswd/{}'.format(service),
+                    user['name'], user['password']])
+        configure_site(
+            '{}.conf'.format(service), 'service.conf',
+            privkey=live['privkey'],
+            fullchain=live['fullchain'],
+            loadbalancing=data['loadbalancing'],
+            service=service,
+            servers=data['private_ips'],
+            fqdns=data['fqdns'],
+            dhparam=live['dhparam'],
+            auth_basic=bool(data['basic_auth']))
     set_state('ssl-termination-proxy.running')
-    status_set('active', '{} have been registered and are online'.format(', '.join(data['fqdns'])))
-
-
-@when('ssl-termination-proxy.running', 'ssltermination.removed')
-def remove_fqdns(ssltermination):
-    data = ssltermination.get_data()[0]
-    db.set('fqdns', list(set(db.get('fqdns')) - set(data['fqdns'])))
-    lets_encrypt.update_fqdns()
+    status_set('active', '{} have been registered and are online'.format(db.get('fqdns')))
